0950d4786e80df8941e1dad19772ee092c94571f
Erick Calder <e@arix.com>
2019-04-21 15:56:08 -0700

    - moved strings to top

--- index.js
+++ index.js
@@ -2,43 +2,6 @@ const pkg = require('./package.json');
 const VER = semVerToInt(pkg.version);
 
 var extensions = {
-    array: {
-        unique() { 
-            return this.filter((e, pos) => this.indexOf(e) == pos);
-        },
-        trim(nonempty = false) {
-            var ret = this;
-            if (nonempty) ret = ret.filter(s => !!s);
-            return ret.map(s => typeof s == 'string' ? s.trim() : s);
-        },
-        flat(depth = 1) {
-            var r = (ret, v) => ret.concat(Array.isArray(v) && depth > 0 ? v.flat(depth - 1) : v);
-            return this.reduce(r, []);
-        },
-        last(n = 0) {
-            return this[this.length - (n + 1)];
-        },
-        unpack() {
-            var l = this.length;
-            return l == 1 ? this[0] 
-                : l == 0 && arguments.length > 0
-                ? undefined
-                : this;
-        },
-        keyval(key = 'k', val = 'v') {
-            var r = (acc, v) => { acc[v[key]] = v[val]; return acc; };
-            return this.reduce(r, {});
-        },
-        isStr() {
-            return false;
-        },
-        isArr() {
-            return true;
-        },
-        isObj() {
-            return false;
-        }
-    },
     string: {
         uc() { 
             return this.toUpperCase(); 
@@ -82,6 +45,43 @@ var extensions = {
         },
         isArr() {
             return false;
+        },
+        isObj() {
+            return false;
+        }
+    },
+    array: {
+        unique() { 
+            return this.filter((e, pos) => this.indexOf(e) == pos);
+        },
+        trim(nonempty = false) {
+            var ret = this;
+            if (nonempty) ret = ret.filter(s => !!s);
+            return ret.map(s => typeof s == 'string' ? s.trim() : s);
+        },
+        flat(depth = 1) {
+            var r = (ret, v) => ret.concat(Array.isArray(v) && depth > 0 ? v.flat(depth - 1) : v);
+            return this.reduce(r, []);
+        },
+        last(n = 0) {
+            return this[this.length - (n + 1)];
+        },
+        unpack() {
+            var l = this.length;
+            return l == 1 ? this[0] 
+                : l == 0 && arguments.length > 0
+                ? undefined
+                : this;
+        },
+        keyval(key = 'k', val = 'v') {
+            var r = (acc, v) => { acc[v[key]] = v[val]; return acc; };
+            return this.reduce(r, {});
+        },
+        isStr() {
+            return false;
+        },
+        isArr() {
+            return true;
         },
         isObj() {
             return false;
