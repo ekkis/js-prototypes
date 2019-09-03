baea1c12ff4c0af57845081850b03ae59a5c6376
Erick Calder <e@arix.com>
2019-09-03 10:24:04 -0700

    - fixed expected json of error

--- t/error.js
+++ t/error.js
@@ -22,8 +22,8 @@ describe('Errors', () => {
 		}
 		catch(e) {
 			var actual = e.json().replace(/\/Users\/\w+/g, '')
-			var expected = '{"stack":"Error: test\\n    at Context.it (/dev/js-prototype-lib/t/error.js:21:10)\\n    at callFn (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runnable.js:387:21)\\n    at Test.Runnable.run (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runnable.js:379:7)\\n    at Runner.runTest (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:525:10)\\n    at /.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:643:12\\n    at next (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:437:14)\\n    at /.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:447:7\\n    at next (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:362:14)\\n    at Immediate._onImmediate (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:415:5)\\n    at processImmediate (timers.js:632:19)","message":"test"}' 
-			assert.equal(actual, expected)
+			var expected = '{"stack":"Error: test\n    at Context.it (/dev/js-prototype-lib/t/error.js:21:10)\n    at callFn (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runnable.js:387:21)\n    at Test.Runnable.run (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runnable.js:379:7)\n    at Runner.runTest (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:525:10)\n    at /.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:643:12\n    at next (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:437:14)\n    at /.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:447:7\n    at next (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:362:14)\n    at Immediate._onImmediate (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:415:5)\n    at processImmediate (timers.js:632:19)","message":"test"}' 
+			assert.equal(actual, expected.replace(/\n/g, '\\n'))
 		}
 	})
 })
