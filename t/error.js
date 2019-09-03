e2708bca547c23908fd3a5705194b1bde76b0f60
Erick Calder <e@arix.com>
2019-09-03 10:29:52 -0700

    - patches to error json test

--- t/error.js
+++ t/error.js
@@ -22,8 +22,9 @@ describe('Errors', () => {
 		}
 		catch(e) {
 			var actual = e.json().replace(/\/Users\/\w+/g, '')
-			var expected = '{"stack":"Error: test\n    at Context.it (/dev/js-prototype-lib/t/error.js:21:10)\n    at callFn (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runnable.js:387:21)\n    at Test.Runnable.run (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runnable.js:379:7)\n    at Runner.runTest (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:525:10)\n    at /.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:643:12\n    at next (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:437:14)\n    at /.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:447:7\n    at next (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:362:14)\n    at Immediate._onImmediate (/.nvm/versions/node/v11.5.0/lib/node_modules/mocha/lib/runner.js:415:5)\n    at processImmediate (timers.js:632:19)","message":"test"}' 
-			assert.equal(actual, expected.replace(/\n/g, '\\n'))
+			assert.ok(typeof actual == 'string', 'did not return a string')
+			assert.ok(actual.match(/^{"stack":"Error: test/, 'does not contain stack'))
+			assert.ok(actual.match(/"message":"test"}$/, 'does not contain message'))
 		}
 	})
 })
